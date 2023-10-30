f373f71c2d0c4804c8e8aca092772a7d01e5f6f5
Alfi74 <catwyzle@ymail.com>
2023-10-30 17:27:22 +0100

    navbar

---
+++ client/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+          <nav id="navigationsleiste" className="schattenmitfarbverlauf">
+            <a href="#">
+              <span id="markenname">Wallet-Wizard</span>
+              <p>@MoCa mit Schuss!</p>
+            </a>
+          </nav>
+    );
+}
