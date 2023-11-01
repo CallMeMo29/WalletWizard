a84c5c422723bd421a269069610b65dad998622b
Alfi74 <catwyzle@ymail.com>
2023-11-01 16:38:35 +0100

    DropdownMenue

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import MenuListe from './DropDown';
 
 export default function Navbar() {
   return (
-          <nav id="navigationsleiste" className="schattenmitfarbverlauf">
-            <a href="#">
-              <span id="markenname">Wallet-Wizard</span>
-              <p>@MoCa mit Schuss!</p>
-            </a>
-          </nav>
+
+    <div id="navigationsleiste" className="container text-center">
+      <div className="row">
+        <div className="col">
+          
+        </div>
+        <div className="col">
+          <a href="#">
+            <span id="markenname">Wallet-Wizard</span>
+            <p>@MoCa mit Schuss!</p>
+          </a>
+        </div>
+        <div className="col">
+          <MenuListe />
+        </div>
+      </div>
+    </div>
     );
 }
