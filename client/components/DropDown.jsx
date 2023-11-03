ba53c5aebd618e84da1676d025fec7ffedb2c81c
CallMeMo29 <addahri.mohamed297@gmail.com>
2023-11-03 15:23:24 +0100

    design

--- client/components/DropDown.jsx
+++ client/components/DropDown.jsx
@@ -1,22 +1,82 @@
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { DropdownButton, Dropdown } from "react-bootstrap";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./CustomDropdown.css"; // Import your custom CSS
 
-class MenuListe extends React.Component{
-    myMenuList={
-        myMenuArray:["Frühling","Sommer","Herbst","Halloween","Winter"]
+const MenuListe = () => {
+  const [showDropdown, setShowDropdown] = useState(false); // State to manage dropdown visibility
+  const [selectedBackground, setSelectedBackground] = useState("");
+  const backgroundClasses = {
+    white: "white-background",
+    spring: "spring-background",
+    summer: "summer-background",
+    autumn: "autumn-background",
+    winter: "winter-background",
+    Neon: "dark-neon-background",
+    // Add other background options as needed
+  };
+
+  const handleBackgroundChange = (selectedBackground) => {
+    setSelectedBackground(selectedBackground);
+    applyBackground(selectedBackground);
+    setShowDropdown(false); // Close the dropdown after selecting an option
+  };
+
+  // Function to apply the selected background to the body element
+  const applyBackground = (selectedBackground) => {
+    if (backgroundClasses[selectedBackground]) {
+      document.body.className = backgroundClasses[selectedBackground];
     }
-    render() {
-        return(
-            <div>            
-                <DropdownButton variant="secondary" title="Wizard-Mode">
-                    {this.myMenuList.myMenuArray.map(data=>(
-                    <Dropdown.Item title="{data}">{data}</Dropdown.Item>
-                    ))}
-                </DropdownButton>
-            </div>
-        );
-    }
-}
+  };
+
+  return (
+    <div className="custom-dropdown">
+      <button
+        className="btn" // Use the custom button class
+        onClick={() => setShowDropdown(!showDropdown)}
+      >
+        <span className="icon">
+          <svg viewBox="0 0 175 80" width="40" height="40">
+            <rect width="80" height="15" fill="#f0f0f0" rx="10"></rect>
+            <rect y="30" width="80" height="15" fill="#f0f0f0" rx="10"></rect>
+            <rect y="60" width="80" height="15" fill="#f0f0f0" rx="10"></rect>
+          </svg>
+        </span>
+        <span className="text">MENU</span>
+      </button>
+      {showDropdown && (
+        <div className="custom-dropdown-menu">
+          {Object.keys(backgroundClasses).map((background) => (
+            <button
+              key={background}
+              className="btn" // Use the custom button class
+              onClick={() => handleBackgroundChange(background)}
+            >
+              <span className="icon">
+                <svg viewBox="0 0 175 80" width="40" height="40">
+                  <rect width="80" height="15" fill="#f0f0f0" rx="10"></rect>
+                  <rect
+                    y="30"
+                    width="80"
+                    height="15"
+                    fill="#f0f0f0"
+                    rx="10"
+                  ></rect>
+                  <rect
+                    y="60"
+                    width="80"
+                    height="15"
+                    fill="#f0f0f0"
+                    rx="10"
+                  ></rect>
+                </svg>
+              </span>
+              <span className="text">{background}</span>
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default MenuListe;
