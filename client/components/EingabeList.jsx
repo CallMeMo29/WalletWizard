e6d3951a8bfcea180c85b62aa62448764fb3fa4f
CallMeMo29 <addahri.mohamed297@gmail.com>
2023-11-03 12:06:50 +0100

    Mo

--- client/components/EingabeList.jsx
+++ client/components/EingabeList.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 const EingabeList = () => {
   const [data, setData] = useState([]);
