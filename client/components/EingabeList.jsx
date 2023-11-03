d6af35a9f5ef6b16c680ae583b16ca0497a6ca1a
Alfi74 <catwyzle@ymail.com>
2023-11-03 11:20:35 +0100

    p2

--- client/components/EingabeList.jsx
+++ client/components/EingabeList.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.css';
-import { Trash } from 'react-bootstrap-icons';
 
 const EingabeList = () => {
   const [data, setData] = useState([]);
@@ -26,7 +25,7 @@ const EingabeList = () => {
       } catch (err) {
       setError(err.message);
       setLoading(false);
-      }
+      }      
     };
 
     fetchData();
@@ -74,7 +73,7 @@ const EingabeList = () => {
                 {item.Datum ? new Date(item.Datum).toLocaleDateString() : ""}
               </span>
               <button className="entfernen-button">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
                   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
                   <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
                 </svg>
