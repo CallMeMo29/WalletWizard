f0f9044d4c2aca815f71ca67cca7f9e9038ec4f6
unknown <onureredo@yahoo.com>
2023-11-07 11:36:40 +0100

    sanity

--- client/components/Eingabeformular.jsx
+++ client/components/Eingabeformular.jsx
@@ -1,47 +1,81 @@
-import React from "react";
 import '../src/index.css';
 
 const Eingabeformular = () => {
-    return (
-        <section id="eingabeformular-container">
-            <form id="eingabeformular" action="#" method="get">
-                <div className="eingabeformular-zeile">
-                    <h1>Neue Einnahme / Ausgabe hinzufügen</h1>
-                </div>
-                <div className="eingabeformular-zeile">
-                    <div className="titel-typ-eingabe-gruppe">
-                        <label htmlFor="titel">Titel</label>
-                        <input type="text" id="titel" form="eingabeformular" name="titel" placeholder="z.B. Einkaufen" size={10} title={"Titel des Eintrags"}/>
-                        <input type='radio' id='einnahme' name='typ' value='einnahme' form='eingabeformular' title={'Typ des Eintrags'}/>
-                        <label htmlFor='einnahme' title={'Typ des Eintrags'}>Einnahme</label>
-                        <input type='radio' id='ausgabe' name='typ' value= 'ausgabe'
-                        form='eingabeformular'
-                        title='Typ des Eintrags'
-                        defaultChecked
-                        />
-                        <label htmlFor="ausgabe" title="Typ des Eintrags">Ausgabe</label> 
-                    </div> 
-                </div> 
-                <div className="eingabeformular-zeile">
-                    <div className="betrag-datum-eingabe-gruppe">
-                        <label htmlFor="betrag">Betrag</label> 
-                        <input type="number" id="betrag" name="betrag"
-                            form="eingabeformular"
-                            placeholder="z.B. 10,42"
-                            size = { 10 }
-                            step= '0.01'
-                            title= 'Betrag des Eintrags (max. zwei Nachkommastellen, kein €-Zeichen)'/> 
-                        <label htmlFor='datum'>Datum</label>
-                        <input type='date' id='datum' name='datum' form='eingabeformular' size={10} title="Datum des Eintrags (Format: tt.mm.jjjj)" />
-                    </div>
-                </div> 
-                <div className="eingabeformular-zeile">
-                    <button className="standard" type="submit" form="eingabeformular">Hinzufügen</button>
-                </div> 
-            </form>
-        </section>
-      
-    );
-  };
-  
-  export default Eingabeformular;
+  return (
+    <section id='eingabeformular-container'>
+      <form id='eingabeformular' action='#' method='get'>
+        <div className='eingabeformular-zeile'>
+          <h1>Neue Einnahme / Ausgabe hinzufügen</h1>
+        </div>
+        <div className='eingabeformular-zeile'>
+          <div className='titel-typ-eingabe-gruppe'>
+            <label htmlFor='titel'>Titel</label>
+            <input
+              type='text'
+              id='titel'
+              form='eingabeformular'
+              name='titel'
+              placeholder='z.B. Einkaufen'
+              size={10}
+              title={'Titel des Eintrags'}
+            />
+            <input
+              type='radio'
+              id='einnahme'
+              name='typ'
+              value='einnahme'
+              form='eingabeformular'
+              title={'Typ des Eintrags'}
+            />
+            <label htmlFor='einnahme' title={'Typ des Eintrags'}>
+              Einnahme
+            </label>
+            <input
+              type='radio'
+              id='ausgabe'
+              name='typ'
+              value='ausgabe'
+              form='eingabeformular'
+              title='Typ des Eintrags'
+              defaultChecked
+            />
+            <label htmlFor='ausgabe' title='Typ des Eintrags'>
+              Ausgabe
+            </label>
+          </div>
+        </div>
+        <div className='eingabeformular-zeile'>
+          <div className='betrag-datum-eingabe-gruppe'>
+            <label htmlFor='betrag'>Betrag</label>
+            <input
+              type='number'
+              id='betrag'
+              name='betrag'
+              form='eingabeformular'
+              placeholder='z.B. 10,42'
+              size={10}
+              step='0.01'
+              title='Betrag des Eintrags (max. zwei Nachkommastellen, kein €-Zeichen)'
+            />
+            <label htmlFor='datum'>Datum</label>
+            <input
+              type='date'
+              id='datum'
+              name='datum'
+              form='eingabeformular'
+              size={10}
+              title='Datum des Eintrags (Format: tt.mm.jjjj)'
+            />
+          </div>
+        </div>
+        <div className='eingabeformular-zeile'>
+          <button className='standard' type='submit' form='eingabeformular'>
+            Hinzufügen
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Eingabeformular;
--- client/components/Herbst.jsx
+++ client/components/Herbst.jsx
@@ -1,49 +1,93 @@
-import "bootstrap/dist/css/bootstrap.css";
-import EingabeList from "./EingabeList";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "../components/Navbar";
-import MoPost from "../components/MoPost";
-import Footer from "../components/Footer";
+/* eslint-disable no-unused-vars */
+import 'bootstrap/dist/css/bootstrap.css';
+import EingabeList from './EingabeList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from '../components/Navbar';
+import MoPost from '../components/MoPost';
+import Footer from '../components/Footer';
 import '../src/index.css';
 
 export default function Herbst() {
-    return (
-        <section className="herbst">
-            <Navbar></Navbar>
-            <MoPost></MoPost>
-            <EingabeList></EingabeList>
-            <Footer></Footer>
-            <div className="set">
-                <div><img src="/public/pics/leaves1.png"></img></div>
-                <div><img src="/public/pics/leaves2.png"></img></div>
-                <div><img src="/public/pics/leaves3.png"></img></div>
-                <div><img src="/public/pics/leaves4.png"></img></div>
-                <div><img src="/public/pics/leaves1.png"></img></div>
-                <div><img src="/public/pics/leaves2.png"></img></div>
-                <div><img src="/public/pics/leaves3.png"></img></div>
-                <div><img src="/public/pics/leaves4.png"></img></div>
-            </div>
-            <div className="set set2">
-                <div><img src="/public/pics/leaves1.png"></img></div>
-                <div><img src="/public/pics/leaves2.png"></img></div>
-                <div><img src="/public/pics/leaves3.png"></img></div>
-                <div><img src="/public/pics/leaves4.png"></img></div>
-                <div><img src="/public/pics/leaves1.png"></img></div>
-                <div><img src="/public/pics/leaves2.png"></img></div>
-                <div><img src="/public/pics/leaves3.png"></img></div>
-                <div><img src="/public/pics/leaves4.png"></img></div>
-            </div>
-            <div className="set set3">
-                <div><img src="/public/pics/leaves1.png"></img></div>
-                <div><img src="/public/pics/leaves2.png"></img></div>
-                <div><img src="/public/pics/leaves3.png"></img></div>
-                <div><img src="/public/pics/leaves4.png"></img></div>
-                <div><img src="/public/pics/leaves1.png"></img></div>
-                <div><img src="/public/pics/leaves2.png"></img></div>
-                <div><img src="/public/pics/leaves3.png"></img></div>
-                <div><img src="/public/pics/leaves4.png"></img></div>
-            </div>
-        </section>
-    );
+  return (
+    <section className='herbst'>
+      <div className='set'>
+        <div>
+          <img src='/public/pics/leaves1.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves2.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves3.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves4.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves1.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves2.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves3.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves4.png'></img>
+        </div>
+      </div>
+      <div className='set set2'>
+        <div>
+          <img src='/public/pics/leaves1.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves2.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves3.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves4.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves1.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves2.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves3.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves4.png'></img>
+        </div>
+      </div>
+      <div className='set set3'>
+        <div>
+          <img src='/public/pics/leaves1.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves2.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves3.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves4.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves1.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves2.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves3.png'></img>
+        </div>
+        <div>
+          <img src='/public/pics/leaves4.png'></img>
+        </div>
+      </div>
+    </section>
+  );
 }
-
